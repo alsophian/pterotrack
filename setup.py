43dc299c00485bf275e4f9d233497d5e0a91f4b2
Michael Jezierny <michael@alsophian.net>
2014-02-24 23:17:51 -0500

    First commit.

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+config = {
+    'description': 'foo',
+    'author': 'Michael Jezierny',
+    'url': 'https://github.com/alsophian/pterotrack',
+    'author_email': 'michael@alsophian.net',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['pterotrack'],
+    'scripts': [],
+    'name': 'Pterotrack'
+}
+
+setup(**config)
---
+++ tests/pterotrack_tests.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+from nose.tools import *
+import ptotrack
+
+
